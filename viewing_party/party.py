8b8aaa08c68935e50ce23cc2e8811111196aeef4
Cady <mcmerkle@gmail.com>
2026-09-14 14:32:42 -0700

    added notes for first two functions in wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,6 +3,29 @@
 def create_movie(title, genre, rating):
     pass
 
+# if title, genre, rating are True
+    # dictionary = { title: value1, genre: value2, rating: value3}
+ # else return None if anything is False
+
+ def add_to_watched(user_data, movie):
+    pass
+
+# if user_data = None, we get -> []
+# user_data = {watched: [{title: "title", "genre": "genre", "rating": "rating"}, {}, {}]}
+# movie = {"title": "Title A","genre": "Horror",rating": 3.5}
+# list = [] <<<< the user has no movies in their watched list
+
+#user_data.append(movie)
+
+#return user_data
+
+def add_to_watchlist(user_data, movie):
+    pass
+
+def watch_movie(user_data, title):
+    pass
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
